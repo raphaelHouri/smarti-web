ded232c28e484fb2963ba518890f4d38b4def723
Raphael <raphaelhouri789@gmail.com>
2025-10-29 09:08:18 +0200

    commit

--- app/lesson/page.tsx
+++ app/lesson/page.tsx
@@ -1,33 +1,7 @@
-// import { getLesson, getUserProgress, getUserSubscriptions } from "@/db/queries";
-// import { redirect } from "next/navigation";
-// import Quiz from "./_components/Quiz";
+import { redirect } from "next/navigation";
 
-// const LessonPage = async() => {
-//     const lessonData =   getLesson();
-//     const userProgressData =  getUserProgress();
-//     const userSubscriptionData = getUserSubscriptions();
+const LessonPage = async () => {
+    redirect("/learn");
+};
 
-
-//     const[lesson,userProgress,userSubscription] = await Promise.all([lessonData,userProgressData,userSubscriptionData]);
-
-    
-//     if(!lesson || !userProgress) {
-//         redirect("/learn");
-//     }
-
-//     const initialPercentage = lesson.challenges
-//     .filter((challenge)=>challenge.completed)
-//     .length / lesson.challenges.length*100;
-
-//     return ( 
-//         <Quiz
-//         initialLessonId = {lesson.id}
-//         initialLessonChallenges = {lesson.challenges}
-//         initialHearts = {userProgress.hearts}
-//         initialPercentage = {initialPercentage}
-//         userSubscription= {userSubscription}
-//         />
-//     );
-// }
-
-// export default LessonPage;
+export default LessonPage;
