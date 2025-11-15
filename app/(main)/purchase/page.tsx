0845de2fe890a1ab1db6142fd1a6e7367f4d8b74
Raphael <raphaelhouri789@gmail.com>
2025-11-15 23:03:35 +0200

    commit

--- app/(main)/purchase/page.tsx
+++ app/(main)/purchase/page.tsx
@@ -1,6 +1,8 @@
 import PurchasePageShop from "../shop/_components/PurchasePageShop";
 import { getPlansForShop } from "@/db/queries";
 
+export const dynamic = "force-dynamic";
+
 export default async function PurchasePage() {
     const plansByType = await getPlansForShop();
     return <PurchasePageShop plansByType={plansByType} />;
