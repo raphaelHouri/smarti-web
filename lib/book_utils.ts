c576e8abc9c26cdae059a750736758787ffdf19b
Raphael <raphaelhouri789@gmail.com>
2025-11-16 15:33:49 +0200

    COMMIT

--- lib/book_utils.ts
+++ lib/book_utils.ts
@@ -2,7 +2,7 @@
 
 export function getFileName(id: string, productType: string): string {
     if (productType) {
-        return `smarti_${productType}_${id}.pdf`;
+        return `smarti_${productType}_${id}_${new Date().getFullYear()}.pdf`;
     }
     return `smarti${id}_${new Date().getDate()}.pdf`;
 }
