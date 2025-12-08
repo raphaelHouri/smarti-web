49c8758c89a76ec64a59b020094cbf43240e82fc
Raphael <raphaelhouri789@gmail.com>
2025-12-08 11:13:56 +0200

    commit

--- app/lesson/_components/Header.tsx
+++ app/lesson/_components/Header.tsx
@@ -20,7 +20,7 @@ const Header = ({
         items-center justify-between mx-auto px-10 flex gap-x-7 w-full">
             <X
                 onClick={open} //Todo:Add exit modal
-                className="text-slate-500 hover:opacity-75 transition cursor-pointer w-8 h-8 sm:w-6 sm:h-6"
+                className="text-slate-500 hover:opacity-75 transition cursor-pointer w-8 h-8 sm:w-8 sm:h-8"
             />
             <Progress value={percentage} />
 
