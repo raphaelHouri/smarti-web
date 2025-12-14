fe2e3d78c9e1f63907b8c962d81d3808378d5b96
Raphael <raphaelhouri789@gmail.com>
2025-12-14 11:52:53 +0200

    commit

--- lib/clerk-localization.ts
+++ lib/clerk-localization.ts
@@ -7,13 +7,16 @@ export const customHeIL = {
         ...heIL.unstable__errors,
         form_identifier_exists__email_address: "כתובת המייל קיימת במערכת. כדי להתחבר, לחצו על הקישור 'התחבר' המופיע למטה.",
     },
-    legalConsent: {
-        ...((heIL as any).legalConsent || {}),
-        checkbox: {
-            ...((heIL as any).legalConsent?.checkbox || {}),
-            label__onlyTermsOfService: 'אני מאשר/ת את {{ termsOfServiceLink || link("תנאי השירות") }}',
-            label__termsOfServiceAndPrivacyPolicy: 'אני מאשר/ת את {{ termsOfServiceLink || link("תנאי השירות") }} ו-{{ privacyPolicyLink || link("מדיניות הפרטיות") }}',
-            label__onlyPrivacyPolicy: 'אני מאשר/ת את {{ privacyPolicyLink || link("מדיניות הפרטיות") }}',
+    signUp: {       //Fix for Hebrew localization of sign up page
+        ...((heIL as any).signUp || {}),
+        legalConsent: {
+            ...((heIL as any).legalConsent || {}),
+            checkbox: {
+                ...((heIL as any).legalConsent?.checkbox || {}),
+                label__onlyTermsOfService: 'אני מאשר/ת את {{ termsOfServiceLink || link("תנאי השירות") }}',
+                label__termsOfServiceAndPrivacyPolicy: 'אני מאשר/ת את {{ termsOfServiceLink || link("תנאי השירות") }} ו-{{ privacyPolicyLink || link("מדיניות הפרטיות") }}',
+                label__onlyPrivacyPolicy: 'אני מאשר/ת את {{ privacyPolicyLink || link("מדיניות הפרטיות") }}',
+            },
         },
     },
 };
