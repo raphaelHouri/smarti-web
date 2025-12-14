a84a2508ef9cce611192696729cec8770d445fce
Raphael <raphaelhouri789@gmail.com>
2025-12-14 11:41:37 +0200

    commit

--- lib/clerk-localization.ts
+++ lib/clerk-localization.ts
@@ -7,6 +7,15 @@ export const customHeIL = {
         ...heIL.unstable__errors,
         form_identifier_exists__email_address: "כתובת המייל קיימת במערכת. כדי להתחבר, לחצו על הקישור 'התחבר' המופיע למטה.",
     },
+    legalConsent: {
+        ...((heIL as any).legalConsent || {}),
+        checkbox: {
+            ...((heIL as any).legalConsent?.checkbox || {}),
+            label__onlyTermsOfService: 'אני מאשר/ת את {{ termsOfServiceLink || link("תנאי השירות") }}',
+            label__termsOfServiceAndPrivacyPolicy: 'אני מאשר/ת את {{ termsOfServiceLink || link("תנאי השירות") }} ו-{{ privacyPolicyLink || link("מדיניות הפרטיות") }}',
+            label__onlyPrivacyPolicy: 'אני מאשר/ת את {{ privacyPolicyLink || link("מדיניות הפרטיות") }}',
+        },
+    },
 };
 
 
