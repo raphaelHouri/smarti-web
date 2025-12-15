c5101f476f9589298e6fc276629e161d6a86c5d5
Raphael <raphaelhouri789@gmail.com>
2025-12-15 10:26:53 +0200

    commit

--- lib/clerk-localization.ts
+++ lib/clerk-localization.ts
@@ -3,6 +3,8 @@ import { heIL } from '@clerk/localizations';
 // Custom Hebrew localization with improved email address error message
 export const customHeIL = {
     ...heIL,
+    formFieldInputPlaceholder__emailAddress: "הכנס כתובת מייל",
+    formFieldInputPlaceholder__password: "הכנס סיסמה",
     unstable__errors: {
         ...heIL.unstable__errors,
         form_identifier_exists__email_address: "כתובת המייל קיימת במערכת. כדי להתחבר, לחצו על הקישור 'התחבר' המופיע למטה.",
