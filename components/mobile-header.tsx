50c4487a5a1ce7f5ab2b786af47b27069dfd4e00
Raphael <raphaelhouri789@gmail.com>
2026-03-07 22:22:42 +0200

    commmit

--- components/mobile-header.tsx
+++ components/mobile-header.tsx
@@ -8,7 +8,7 @@ export const MobileHeader = ({ hasManaged = false }: MobileHeaderProps) => {
     return (
         <nav
             className="
-        lg:hidden px-2 top-10  w-full bg-green-500
+        lg:hidden px-2 top-0  w-full bg-green-500
         h-[50px] flex fixed items-center border-b z-50
         ">
             <MobileSideBar hasManaged={hasManaged} />
